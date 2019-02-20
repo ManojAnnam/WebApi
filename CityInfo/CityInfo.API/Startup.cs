113d8eb697e01ef0c231d6aba476372c9d448165
Manoj Annam <manoj.annam@ggktech.com>
2019-02-20 13:12:13 +0530

    Added Output Formatters

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -18,12 +19,13 @@ namespace CityInfo.API
         {
             services.AddMvc().AddJsonOptions(o =>
             {
-                if(o.SerializerSettings.ContractResolver !=null)
+                if (o.SerializerSettings.ContractResolver != null)
                 {
                     var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
                     castedResolver.NamingStrategy = null;
                 }
-            });
+            }).AddMvcOptions(o => o.OutputFormatters.Add(
+                new XmlDataContractSerializerOutputFormatter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
